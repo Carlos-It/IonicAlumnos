9e7c54016f77a640287c5505df5884faa9242693
Carlos-It <carlos.iturbide.tic@gmail.com>
2019-12-05 19:15:37 +0100

    Constructor alumno.model Y fecha.model.ts

--- src/app/models/alumno.model.ts
+++ src/app/models/alumno.model.ts
@@ -1,16 +1,39 @@
+import { Fechamodel } from './fecha.model';
+
 export enum Sexo { Hombre, Mujer };
 export class AlumnoModel {
 
     private _nombre: string;
     private _apellidos: string;
-    private _F_Nac: Date;
+    private _F_Nac: Fechamodel;
     private _sexo: Sexo;
     private _email: string;
     private _telefono: number;
     private _foto: string;
 
 
-    constructor() { }
+    constructor(nombre: string, apellidos: string, fNac?: number[], sexo?: Sexo,
+        email?: string, telefono?: number, foto?: string) {
+
+        this._nombre = nombre;
+        this._apellidos = apellidos;
+        if (fNac) {
+            this._F_Nac = new Fechamodel(fNac);
+        }
+        if (sexo) {
+            this._sexo = sexo;
+        }
+        if (email) {
+            this._email = email;
+        }
+        if (telefono) {
+            this._telefono = telefono;
+        }
+        if (foto) {
+            this._foto = foto;
+        }
+
+    }
 
     public get nombre(): string {
         return this._nombre;
@@ -18,10 +41,10 @@ export class AlumnoModel {
     public set nombre(value: string) {
         this._nombre = value;
     }
-    public get F_Nac(): Date {
+    public get F_Nac(): Fechamodel {
         return this._F_Nac;
     }
-    public set F_Nac(value: Date) {
+    public set F_Nac(value: Fechamodel) {
         this._F_Nac = value;
     }
 
@@ -57,5 +80,4 @@ export class AlumnoModel {
     public set foto(value: string) {
         this._foto = value;
     }
-    // public get F_Nac = () => `${this._F_Nac.getDay} / ${this._F_Nac.getMonth} / ${this._F_Nac.getFullYear} `
 }
---
+++ src/app/models/fecha.model.ts
@@ -0,0 +1,54 @@
+export class Fechamodel {
+
+    private _dia: number;
+    private _mes: number;
+    private _anio: number;
+/*
+     constructor(dia: number, mes: number, anio: number) {
+         this._dia = dia;
+         this._mes = mes;
+         this._anio = anio;
+     }
+
+     constructor(fecha:number[]) {
+        this._dia=fecha[0];
+        this._mes=fecha[1];
+        this._anio=fecha[2];
+     }
+
+*/
+    constructor(fecha:number[]) {
+        this._dia=fecha[0];
+        this._mes=fecha[1];
+        this._anio=fecha[2];
+     }
+
+    public get dia(): number {
+        return this._dia;
+    }
+    public set dia(value: number) {
+        if (value > 0 && value < 32) {
+            this._dia = value;
+        }
+    }
+
+    public get mes() {
+        return this._mes;
+    }
+
+    public set mes(value: number) {
+        if (value > 0 && value < 13) {
+            this._mes = value;
+        }
+    }
+
+    public get anio(): number {
+        return this._anio;
+    }
+
+    public set anio(value: number) {
+        if (value > 0) {
+            this._anio = value;
+        }
+    }
+}
